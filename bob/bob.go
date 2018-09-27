249fa1cbb202dad629f0af97f98e28575ef70304
Matt Boersma <matt@sprout.org>
2018-09-27 13:45:44 -0600

    [bob] all tests passing

--- bob/bob.go
+++ bob/bob.go
@@ -1,15 +1,40 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package bob should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
+// Package bob responds to conversation as a lackadaisical teenager would.
 package bob
 
-// Hey should have a comment documenting it.
+import (
+	"strings"
+	"unicode"
+)
+
+// HasLetter returns whether or not a string contains an alphabetic character.
+func HasLetter(s string) bool {
+	for _, r := range s {
+		if unicode.IsLetter(r) {
+			return true
+		}
+	}
+	return false
+}
+
+// Hey returns a lackadaisical teenager's response to the remark provided.
 func Hey(remark string) string {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return ""
+	remark = strings.TrimSpace(remark)
+
+	if remark == "" {
+		return "Fine. Be that way!"
+	}
+
+	yell := HasLetter(remark) && strings.ToUpper(remark) == remark
+	question := strings.HasSuffix(remark, "?")
+
+	if yell && question {
+		return "Calm down, I know what I'm doing!"
+	}
+	if question {
+		return "Sure."
+	}
+	if yell {
+		return "Whoa, chill out!"
+	}
+	return "Whatever."
 }
