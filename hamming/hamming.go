af0292ad4de90bd6e3267255d42a1366cec5328b
Matt Boersma <matt@sprout.org>
2019-03-13 17:18:29 -0600

    [hamming] all tests passing

--- hamming/hamming.go
+++ hamming/hamming.go
@@ -1,4 +1,21 @@
+// Package hamming calculates the Hamming distance between two DNA strands.
 package hamming
 
+import "errors"
+
+// Distance returns how many characters differ between two strings in the same
+// locations, also known as the Hamming distance.
 func Distance(a, b string) (int, error) {
+	if len(a) != len(b) {
+		return 0, errors.New("strings must be the same length")
+	}
+
+	distance := 0
+	for i := 0; i < len(a); i++ {
+		if a[i] != b[i] {
+			distance++
+		}
+	}
+
+	return distance, nil
 }
