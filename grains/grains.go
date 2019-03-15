3b4bdc61657c84d17cf8181c2492ed1d1f774da2
Matt Boersma <matt@sprout.org>
2019-03-15 12:59:21 -0600

    [grains] better implementation

--- grains/grains.go
+++ grains/grains.go
@@ -4,7 +4,6 @@ package grains
 
 import (
 	"errors"
-	"math"
 )
 
 // Square returns two to the power of the provided integer.
@@ -12,15 +11,10 @@ func Square(n int) (uint64, error) {
 	if n < 1 || n > 64 {
 		return 0, errors.New("value must be between 1 and 64, inclusive")
 	}
-	return uint64(math.Exp2(float64(n - 1))), nil
+	return 1 << uint64(n-1), nil
 }
 
 // Total sums the number of grains on all 64 squares of the chessboard.
 func Total() uint64 {
-	var total uint64
-	for i := 1; i <= 64; i++ {
-		square, _ := Square(i)
-		total += square
-	}
-	return total
+	return 1<<64 - 1
 }
