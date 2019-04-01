a3d8544622b05ee38b3fe2a299bb09a336d30bc9
Matt Boersma <matt@sprout.org>
2019-04-01 13:28:46 -0600

    [parallel-letter-frequency] all tests passing

--- parallel-letter-frequency/frequency.go
+++ parallel-letter-frequency/frequency.go
@@ -1,3 +1,4 @@
+// Package letter counts the frequency of letters in texts using parallel computation.
 package letter
 
 // FreqMap records the frequency of each rune in a given text.
@@ -11,4 +12,23 @@ func Frequency(s string) FreqMap {
 		m[r]++
 	}
 	return m
+}
+
+// ConcurrentFrequency counts the frequency of each rune in given texts and returns
+// this data as a FreqMap. Each text is analyzed concurrently.
+func ConcurrentFrequency(list []string) FreqMap {
+	ch := make(chan FreqMap, len(list))
+	for _, s := range list {
+		go func(s string) {
+			ch <- Frequency(s)
+		}(s)
+	}
+
+	results := FreqMap{}
+	for range list {
+		for letter, freq := range <-ch {
+			results[letter] += freq
+		}
+	}
+	return results
 }
