0507108117181ca0bea37735d854e9f5c155596a
Matt Boersma <matt@sprout.org>
2020-09-04 11:48:02 -0600

    [acronym] updated to latest exercism revision

--- acronym/acronym.go
+++ acronym/acronym.go
@@ -8,7 +8,7 @@ import (
 // Abbreviate shortens a noun phrase into its acronym.
 func Abbreviate(s string) string {
 	var acronym string
-	s = strings.ToUpper(strings.Replace(s, "-", " ", -1))
+	s = strings.Replace(strings.Replace(s, "_", " ", -1), "-", " ", -1)
 	words := strings.Fields(s)
 	for _, w := range words {
 		acronym += string(w[0])
