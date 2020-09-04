a811fb6380df0268f4884059d0ae5d71f44191bb
Matt Boersma <matt@sprout.org>
2020-09-04 11:57:41 -0600

    [reverse-string] updated to latest exercism revision

--- reverse-string/reverse_string.go
+++ reverse-string/reverse_string.go
@@ -1,8 +1,8 @@
 // Package reverse reverses a string.
 package reverse
 
-// String returns a string reversed.
-func String(s string) string {
+// Reverse returns a string reversed.
+func Reverse(s string) string {
 	r := []rune(s)
 	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
 		r[i], r[j] = r[j], r[i]
