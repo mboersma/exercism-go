5cb0a99b4841b36ee3142717cb1129ce72645486
Matt Boersma <matt@sprout.org>
2020-09-04 11:54:22 -0600

    [hamming] updated to latest exercism revision

--- hamming/hamming_test.go
+++ hamming/hamming_test.go
@@ -15,16 +15,16 @@ func TestHamming(t *testing.T) {
 					tc.s1, tc.s2)
 			}
 		} else {
-			if got != tc.want {
-				t.Fatalf("Distance(%q, %q) = %d, want %d.",
-					tc.s1, tc.s2, got, tc.want)
-			}
-
 			// we do not expect error
 			if err != nil {
 				t.Fatalf("Distance(%q, %q) returned unexpected error: %v",
 					tc.s1, tc.s2, err)
 			}
+			if got != tc.want {
+				t.Fatalf("Distance(%q, %q) = %d, want %d.",
+					tc.s1, tc.s2, got, tc.want)
+			}
+
 		}
 	}
 }
@@ -33,7 +33,8 @@ func BenchmarkHamming(b *testing.B) {
 	// bench combined time to run through all test cases
 	for i := 0; i < b.N; i++ {
 		for _, tc := range testCases {
-			Distance(tc.s1, tc.s2)
+			// ignoring errors and results because we're just timing function execution
+			_, _ = Distance(tc.s1, tc.s2)
 		}
 	}
 }
