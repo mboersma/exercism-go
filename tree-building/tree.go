3fa707c4438c2049de11f74b7defb6fcb86392e6
Matt Boersma <matt@sprout.org>
2020-09-06 11:50:49 -0600

    [tree-building] happy tests passing

--- tree-building/tree.go
+++ tree-building/tree.go
@@ -2,7 +2,7 @@
 package tree
 
 import (
-	"fmt"
+	"sort"
 )
 
 // Node is an element of the tree that may have child nodes.
@@ -20,27 +20,27 @@ type Record struct {
 // Build creates a tree of nodes from a list of input records.
 func Build(records []Record) (*Node, error) {
 	var root *Node = nil
-	nodes := map[int]Node{}
+	nodes := map[int]*Node{}
 	for _, record := range records {
 		id, parentId := record.ID, record.Parent
 		node, ok := nodes[id]
 		if !ok {
-			node = Node{ID: id}
+			node = &Node{ID: id}
 			nodes[id] = node
 		}
 		if id == parentId {
-			root = &node
+			root = node
 			continue
 		}
 		parent, ok := nodes[parentId]
 		if !ok {
-			parent = Node{ID: parentId}
+			parent = &Node{ID: parentId}
 			nodes[parentId] = parent
 		}
-		parent.Children = append(parent.Children, &node)
-		fmt.Printf("%v\n", parent)
+		parent.Children = append(parent.Children, node)
+		sort.Slice(parent.Children, func(i, j int) bool {
+			return parent.Children[i].ID < parent.Children[j].ID
+		})
 	}
-	fmt.Printf("%v\n", nodes)
-	fmt.Printf("%v\n", root)
 	return root, nil
 }
